440f9a428a9effbe22cc43adb0ed956a910292c3
Tarsum67 <taraum@icloud.com>
2023-11-19 18:16:59 -0600

    updated index

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,21 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
